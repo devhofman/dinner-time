2f1e54d3893d5626dbb5d46a31d34b31aa521276
devhofman <48828521+devhofman@users.noreply.github.com>
2020-02-16 21:29:11 +0100

    api - auth register and login in progress

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/recipes', 'RecipesController@index');
+Route::post('/recipes', 'RecipesController@store');
+Route::patch('/recipes/{recipe}', 'RecipesController@update');
+Route::delete('/recipes/{recipe}', 'RecipesController@destroy');
+
+Route::post('/register', 'AuthController@register');
+Route::post('/login', 'AuthController@login');
